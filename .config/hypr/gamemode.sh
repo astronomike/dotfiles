ace602940756b8bab96da6306b3c9827d6fc3c2c
Michael Sarkis <michael.sarkis@students.wits.ac.za>
2023-10-11 19:21:45 +0200

    small waybar fixes (multi-mon, idle_inhib timeout)

--- .config/hypr/gamemode.sh
+++ .config/hypr/gamemode.sh
@@ -6,7 +6,7 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
         keyword decoration:drop_shadow 0;\
         keyword decoration:blur:enabled 0;\
         keyword general:gaps_in 0;\
-        keyword general:gaps_out 0;\
+        keyword general:gaps_out 10;\
         keyword general:border_size 0;\
         keyword decoration:rounding 0"
     exit
